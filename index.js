f7556373d0ed53217debbae2a3cc04201c8140bc
liztanyl <elizabeth.tanyulin@gmail.com>
2022-02-08 11:45:37 +0800

    add console log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-import { writeFile } from 'fs';
+import { writeFile } from "fs";
 let name = process.argv[2];
-writeFile('data.json', name, (err) => console.error('err ->', err));
+writeFile("data.json", name, (err) => console.error("err ->", err));
 
-
+console.log(`yo ${name}, what's up dude.`);
